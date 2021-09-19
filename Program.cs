fc02972ff6d5dce684f36fb9b7058e65bd8acd63
topu9 <topu9@DESKTOP-K80NHHD>
2021-09-19 21:41:52 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MobileBillCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime peackHourStart = new DateTime(2019, 08, 31, 09, 00, 00);
+            DateTime peackHourEnd = new DateTime(2019, 08, 31, 22, 59, 59);
+
+            DateTime starttime;
+            DateTime endtime;
+            Console.WriteLine("Start Time: ");
+            starttime = DateTime.Parse(Console.ReadLine());
+            
+            Console.WriteLine("\nEnd Time: ");
+            endtime = DateTime.Parse(Console.ReadLine());
+            TimeSpan timeDifference = endtime.Subtract(starttime);
+            double TotalCallTime = timeDifference.TotalSeconds / 20;
+            decimal mobileBill = 0;
+            for (int i = 0; i < Math.Ceiling(TotalCallTime); i++)
+            {
+                DateTime starttime1 = starttime.AddSeconds(20);
+                if (starttime1 >= peackHourStart && starttime1 <= peackHourEnd)
+                {
+                    mobileBill += 30;
+                }
+                else
+                {
+                    mobileBill += 20;
+                }
+                starttime = starttime1;
+            }
+            Console.WriteLine("\nTotal Bill: " + mobileBill/100 + " Taka");
+            Console.ReadKey();
+
+        }
+    }
+}
